c75b2d01fbfc3ed4bda79870857cfbb1ed4aae04
milldr <miller0daniel@gmail.com>
2025-04-15 14:47:42 -0400

    upstream and readme

--- src/outputs.tf
+++ src/outputs.tf
@@ -1,4 +1,4 @@
-output "mock" {
-  description = "Mock output example for the Cloud Posse Terraform component template"
-  value       = local.enabled ? "hello ${basename(abspath(path.module))}" : ""
+output "id" {
+  description = "The ID of the component"
+  value       = module.this.id
 }
---
+++ src/remote-state.tf
@@ -0,0 +1,35 @@
+variable "spacelift_spaces_environment_name" {
+  type        = string
+  description = "The environment name of the spacelift spaces component"
+  default     = null
+}
+
+variable "spacelift_spaces_stage_name" {
+  type        = string
+  description = "The stage name of the spacelift spaces component"
+  default     = null
+}
+
+variable "spacelift_spaces_tenant_name" {
+  type        = string
+  description = "The tenant name of the spacelift spaces component"
+  default     = null
+}
+
+variable "spacelift_spaces_component_name" {
+  type        = string
+  description = "The component name of the spacelift spaces component"
+  default     = "spacelift/spaces"
+}
+
+module "spaces" {
+  source  = "cloudposse/stack-config/yaml//modules/remote-state"
+  version = "1.5.0"
+
+  component   = var.spacelift_spaces_component_name
+  environment = try(var.spacelift_spaces_environment_name, module.this.environment)
+  stage       = try(var.spacelift_spaces_stage_name, module.this.stage)
+  tenant      = try(var.spacelift_spaces_tenant_name, module.this.tenant)
+
+  context = module.this.context
+}
